4e45d627eb871714e67b15eee57ae3613b094c7f
Benz <fuckoff@email>
2018-08-16 05:15:00 -0400

    Add project files.

---
+++ TPLDataflow Task Management/BlockScheduler.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace TPLDataflow_Task_Management
+{
+    public class Capsule<T, T1>
+    {
+        public IPropagatorBlock<T, T> Beginning { get; private set; }
+        public IPropagatorBlock<T1, T1> Ending { get; private set; }
+        public Capsule(IPropagatorBlock<T, T> Beginning, IPropagatorBlock<T1, T1> Ending)
+        {
+            this.Beginning = Beginning;
+            this.Ending = Ending;
+        }
+    }
+    public class BlockScheduler<T>
+    {
+        private BufferBlock<T> Queue { get; set; }
+        private List<Task> Consumers { get; set; }
+        private CancellationTokenSource BlockCTS { get; set; }
+
+        private IPropagatorBlock<T1, T1> CreateThrottleBlock<T1>(TimeSpan ScheduledAt, CancellationTokenSource CTS, int MaxPerInterval, int BlockMaxDegreeOfParallelism)
+        {
+            SemaphoreSlim sem = new SemaphoreSlim(MaxPerInterval, MaxPerInterval);
+            return new TransformBlock<T1, T1>(async (x) =>
+            {
+                await sem.WaitAsync();
+                while (ScheduledAt >= DateTime.UtcNow.TimeOfDay && !CTS.Token.IsCancellationRequested)
+                {
+                    await Task.Delay(1000);
+                }
+                sem.Release();
+                return x;
+            },
+                 new ExecutionDataflowBlockOptions
+                 {
+                     BoundedCapacity = MaxPerInterval,
+                     MaxDegreeOfParallelism = BlockMaxDegreeOfParallelism,
+                     EnsureOrdered = true,
+                     CancellationToken = CTS.Token
+                 });
+        }
+        /// <summary>
+        /// Create a queue of block encapsuled by throttling and custom consumer.
+        /// Limitations: The first block and last block(consumer) must have same in/out types.
+        /// </summary>
+        /// <param name="Capsule"></param>
+        /// <param name="ConsumerAction"></param>
+        /// <param name="ScheduledAt"></param>
+        /// <param name="MaxDegreeOfParallelism"></param>
+        /// <param name="BoundedCapacity"></param>
+        public BlockScheduler(Capsule<T, T> Capsule, Func<T, Task> ConsumerAction, TimeSpan ScheduledAt, int MaxDegreeOfParallelism, int BoundedCapacity)
+        {
+            BlockCTS = new CancellationTokenSource();
+
+            var ConsumerOptions = new ExecutionDataflowBlockOptions { BoundedCapacity = BoundedCapacity, MaxDegreeOfParallelism = MaxDegreeOfParallelism, EnsureOrdered = true, CancellationToken = BlockCTS.Token };
+            var LinkOptions = new DataflowLinkOptions { PropagateCompletion = true, };
+            Consumers = new List<Task>();
+            Queue = new BufferBlock<T>();
+
+            var Consumer = new ActionBlock<T>(async TT => await ConsumerAction(TT), ConsumerOptions);
+
+            var ThrottleBlock = CreateThrottleBlock<T>(ScheduledAt, BlockCTS, BoundedCapacity, MaxDegreeOfParallelism);
+            Queue.LinkTo(ThrottleBlock, LinkOptions);
+
+
+            ThrottleBlock.LinkTo(Capsule.Beginning, LinkOptions);
+            Capsule.Ending.LinkTo(Consumer, LinkOptions);
+
+            for (int i = 0; i < MaxDegreeOfParallelism; i++)
+            {
+                Consumers.Add(Consumer.Completion);
+            }
+        }
+        public async Task SendAsync(T Item)
+        {
+            await Queue.SendAsync(Item);
+        }
+
+        public async Task SendAsync(IEnumerable<T> Items)
+        {
+            foreach (var item in Items)
+            {
+                await Queue.SendAsync(item);
+            }
+        }
+
+        public async Task CompleteAsync()
+        {
+            Queue.Complete();
+            await Task.WhenAll(Consumers);
+        }
+        public async Task CancelAll()
+        {
+            BlockCTS.Token.ThrowIfCancellationRequested();
+            await Task.Delay(6000);
+            BlockCTS.Cancel();
+        }
+    }
+}
---
+++ TPLDataflow Task Management/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TPLDataflow_Task_Management
+{
+    //public class TaskResult
+    //{
+    //    public CancellationTokenSource CTS { get; set; }
+    //    public TaskCompletionSource<string> TCS { get; set; }
+    //    public string Id { get; set; }
+    //    public TaskResult(string Id, CancellationTokenSource CTS)
+    //    {
+    //        this.Id = Id;
+    //        this.CTS = CTS;
+    //        TCS = new TaskCompletionSource<string>();
+    //    }
+    //}
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>   
+    public class Rabbit
+    {
+        public string Lorem { get; set; }
+    }
+    public partial class MainWindow : Window
+    {
+        public async Task Consumer(string T)
+        {
+
+            Console.WriteLine(" {0} wearing shorts.", T);
+            await Task.Delay(5000);
+
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var blockOptions = new ExecutionDataflowBlockOptions()
+            {
+                MaxDegreeOfParallelism = 1,
+                BoundedCapacity = 1,
+                EnsureOrdered = true
+            };
+            var LinkOptions = new DataflowLinkOptions()
+            {
+                PropagateCompletion = true,
+            };
+
+            var FirstBlock = new TransformBlock<string, string>(T =>
+            {
+                return $" {T} rabbits";
+            }, blockOptions);
+
+            var SecondBlock = new TransformBlock<string, Rabbit>(T =>
+            {
+                return new Rabbit() { Lorem = $"{T} are" };
+            }, blockOptions);
+            var ThirdBlock = new TransformBlock<Rabbit, string>(T =>
+            {
+                return $" {T.Lorem} faster than";
+            }, blockOptions);
+
+            var FourthBlock = new TransformBlock<string, string>(T =>
+            {
+                return $" {T} your mother";
+            }, blockOptions);
+            FirstBlock.LinkTo(SecondBlock, LinkOptions);
+            SecondBlock.LinkTo(ThirdBlock, LinkOptions);
+            ThirdBlock.LinkTo(FourthBlock, LinkOptions);
+            //cts.Cancel();
+            BlockScheduler<string> blockScheduler = new BlockScheduler<string>(new Capsule<string, string>(FirstBlock, FourthBlock), Consumer, DateTime.UtcNow.TimeOfDay, 1, 1);
+            for (int i = 0; i < 20; i++)
+            {
+                blockScheduler.SendAsync(i.ToString());
+
+            }
+            blockScheduler.CancelAll();
+
+            //Consumer.Complete();
+            //Queue.Completion.Wait();
+            //Queue.Complete();
+            //Consumer.Completion.Wait();
+        }
+    }
+}
